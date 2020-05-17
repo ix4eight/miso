aca7c51985f2e4fb78f9c1e78506f6cf5fa23410
ix4eight <ix4eight@gmail.com>
2020-05-18 01:29:07 +0900

    form_with update

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -4,9 +4,15 @@ class ApplicationsController < ApplicationController
   end
 
   def new
+    @library = Library.new
   end
 
   def create
+
   end
 
+  private
+  def library_params
+    params.require(:library).permit(:twitter, :line, :instagram, :discord, :steam, :skype).merge(user_id: current_user.id)
+  end
 end
